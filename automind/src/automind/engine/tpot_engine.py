5986be1628c9597684a20f5945e529f5b6716a0f
Abner <qw0207060413@gmail.com>
2025-12-23 02:59:47 +0800

    feat: tpot engine for mindsdb

--- automind/src/automind/engine/tpot_engine.py
+++ automind/src/automind/engine/tpot_engine.py
@@ -1,9 +1,12 @@
+import json
 import os
 import pickle
-from typing import Optional
+from typing import List, Optional
 
 from pandas import DataFrame
-from tpot import TPOTClassifier, TPOTRegressor
+from sklearn.metrics import get_scorer
+from sklearn.model_selection import train_test_split
+from tpot.tpot import TPOTClassifier, TPOTRegressor
 
 # refs:
 # https://docs.mindsdb.com/integrations/ai-engines/byom
@@ -14,7 +17,14 @@ from tpot import TPOTClassifier, TPOTRegressor
 class TPOTEngine:
     def __init__(self):
         self.model = None
+        self.task_type = None
+        self.classification_scorer_names = []
+        self.regression_scorer_names = []
+        self.input_features: List[str] = []
+        self.output_features: List[str] = []
         self.model_path = "tpot_best_pipeline.pkl"
+        self.scorer_names: List[str] = []
+        self.scores = {}
 
     def train(self, df: DataFrame, target_col: str, args=None):
         # Setup default args
@@ -22,24 +32,50 @@ class TPOTEngine:
         mode = args.get("mode", "classification").lower()
         max_time_mins = args.get("max_time_mins", 5)
 
+        self.task_type = mode
+
+        # https://scikit-learn.org/stable/modules/model_evaluation.html#string-name-scorers
+        self.classification_scorer_names = args.get(
+            "classification_scorers", "accuracy,f1"
+        ).split(",")
+        self.regression_scorerss = args.get(
+            "regression_scorers", "neg_root_mean_squared_error"
+        ).split(",")
+
         # Data preparation
         y = df[target_col]
         X = df.drop(columns=[target_col])
+
+        self.input_features = X.columns.tolist()
+        self.output_features = [target_col]
 
         # simple transformation to ensure model be able to run
         for col in X.select_dtypes(include=["object", "category"]).columns:
             X[col] = X[col].astype("category").cat.codes
 
+        X_train, X_validation, y_train, y_validation = train_test_split(
+            X, y, train_size=0.75, test_size=0.25
+        )
+
         # Initialize automl engine
-        tpot = None
-        if mode == "regression":
-            tpot = TPOTRegressor(max_time_mins=max_time_mins)
+        est = None
+
+        if self.task_type == "regression":
+            est = TPOTRegressor(max_time_mins=max_time_mins)
+            self.scorer_names = self.regression_scorer_names
         else:
-            tpot = TPOTClassifier(max_time_mins=max_time_mins)
+            est = TPOTClassifier(max_time_mins=max_time_mins)
+            self.scorer_names = self.classification_scorer_names
 
-        tpot.fit(X, y)
+        est.fit(X_train, y_train)
 
-        self.model = tpot.fitted_pipeline_
+        # Validation
+        for scorer_name in self.scorer_names:
+            scorer = get_scorer(scorer_name)
+            score = scorer(est, X_validation, y_validation)
+            self.scores[scorer_name] = score
+
+        self.model = est.fitted_pipeline_  # # pyright: ignore
 
         # Save model
         with open(self.model_path, "wb") as f:
@@ -69,9 +105,16 @@ class TPOTEngine:
 
         return output_df
 
-    def describe(self, model_state, attribute: Optional[str] = None):
+    def describe(self, attribute: Optional[str] = None):
         if attribute == "info":
-            return DataFrame()
+            description = {
+                "task_type": [self.task_type],
+                "input": [json.dumps(self.input_features)],
+                "output": [json.dumps(self.output_features)],
+                "score_names": [json.dumps(self.scorer_names)],
+                "scores": [json.dumps(self.scores)],
+            }
+            return DataFrame.from_dict(description)
         else:
             tables = ["info"]
             return DataFrame(tables, columns=["tables"])
